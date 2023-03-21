e0528ef9fa564713ce840d38590ac2d0cd72cead
Thierry GRN <83121294+Rycks2010@users.noreply.github.com>
2023-03-21 09:45:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+        // Script to open and close sidebar when on tablets and phones
+        function w3_open() {
+            document.getElementById("mySidebar").style.display = "block";
+            document.getElementById("myOverlay").style.display = "block";
+        }
+
+        function w3_close() {
+            document.getElementById("mySidebar").style.display = "none";
+            document.getElementById("myOverlay").style.display = "none";
+        }
+
+       
